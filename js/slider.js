a17c8bc5db7ec4b3148046e3dcb146652c40b5ab
Alain Peralta <104697852+Alain5595@users.noreply.github.com>
2025-02-01 21:52:12 -0500

    I customized animation

--- js/slider.js
+++ js/slider.js
@@ -1,24 +1,39 @@
 (function(){
-    
     const sliders = [...document.querySelectorAll('.testimony__body')];
     const buttonNext = document.querySelector('#next');
     const buttonBefore = document.querySelector('#before');
-    let value;   
+    let value;
 
-    buttonNext.addEventListener('click', ()=>{
+    buttonNext.addEventListener('click', () => {
         changePosition(1);
     });
-    buttonBefore.addEventListener('click', ()=>{
+
+    buttonBefore.addEventListener('click', () => {
         changePosition(-1);
     });
-    const changePosition = (add)=>{
+
+    const changePosition = (add) => {
         const currentTestimony = document.querySelector('.testimony__body--show').dataset.id;
         value = Number(currentTestimony);
-        value+= add;
-        sliders[Number(currentTestimony)-1].classList.remove('testimony__body--show');
-        if(value === sliders.length+1 || value === 0){
-            value = value === 0 ? sliders.length  : 1;
+        value += add;
+
+        sliders[Number(currentTestimony) - 1].classList.remove("testimony__body--show");
+
+        if (value > sliders.length) {
+            value = 1;
+        } else if (value === 0) {
+            value = sliders.length;
         }
-        sliders[value-1].classList.add('testimony__body--show');
-    }
+
+        sliders[value - 1].classList.add("testimony__body--show");
+    };
+
+    // Función para cambiar automáticamente cada 5 segundos
+    const autoSlide = () => {
+        setInterval(() => {
+            changePosition(1);
+        }, 12000); // Cambia cada 5 segundos
+    };
+
+    autoSlide(); // Inicia el slider automático
 })();
